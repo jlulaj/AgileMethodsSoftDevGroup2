844e38dde2ac82a55cf35732b22aa7f245caaf85
jbaldi13 <jbaldi13@optimum.net>
2023-06-12 22:38:03 -0400

    Update readGed.py
    Put the printing of individual and family tables in a main function

--- readGed.py
+++ readGed.py
@@ -65,70 +65,74 @@ def extract_families(gedcom):
     return families
 
 
+def main():
+    # Read the GEDCOM file
+    with open("Family_Tree_GEDCOM.ged", "r") as file:
+        gedcom_data = file.readlines()
 
-# Read the GEDCOM file
-with open("Family_Tree_GEDCOM.ged", "r") as file:
-    gedcom_data = file.readlines()
+    # Convert gedcom_data into an iterator
+    gedcom_iterator = iter(gedcom_data)
 
-# Convert gedcom_data into an iterator
-gedcom_iterator = iter(gedcom_data)
+    individuals = extract_individuals(gedcom_iterator)
 
-individuals = extract_individuals(gedcom_iterator)
+    gedcom_iterator = iter(gedcom_data)
+    families = extract_families(gedcom_iterator)
 
-gedcom_iterator = iter(gedcom_data)
-families = extract_families(gedcom_iterator)
+    # Display individual information in a table
+    individual_table = PrettyTable()
+    individual_table.field_names = ["ID", "Name", "Gender", "Age", "Alive", "Death", "Child", "Spouse"]
 
-# Display individual information in a table
-individual_table = PrettyTable()
-individual_table.field_names = ["ID", "Name", "Gender", "Age", "Alive", "Death", "Child", "Spouse"]
+    for indi_id, indi_info in individuals.items():
+        indi_table_row = [indi_id, indi_info["Name"], indi_info["Gender"]]
+        if "Birth Date" in indi_info:
+            birth_date = datetime.datetime.strptime(indi_info["Birth Date"], "%d %b %Y")
+            age = calculate_age(birth_date)
+            indi_table_row.append(age)
+        else:
+            indi_table_row.append("N/A")
+        if indi_info["Alive"]:
+            indi_table_row.append("Yes")
+            indi_table_row.append("-")
+        else:
+            indi_table_row.append("No")
+            indi_table_row.append(indi_info["Death Date"])
+        child = next((fam_id for fam_id, fam_info in families.items() if indi_id in fam_info.get("Children", [])), "-")
+        indi_table_row.append(child)
+        spouse = next((fam_id for fam_id, fam_info in families.items() if indi_id == fam_info.get("Husband ID") or indi_id == fam_info.get("Wife ID")), "-")
+        indi_table_row.append(spouse)
+        individual_table.add_row(indi_table_row)
 
-for indi_id, indi_info in individuals.items():
-    indi_table_row = [indi_id, indi_info["Name"], indi_info["Gender"]]
-    if "Birth Date" in indi_info:
-        birth_date = datetime.datetime.strptime(indi_info["Birth Date"], "%d %b %Y")
-        age = calculate_age(birth_date)
-        indi_table_row.append(age)
-    else:
-        indi_table_row.append("N/A")
-    if indi_info["Alive"]:
-        indi_table_row.append("Yes")
-        indi_table_row.append("-")
-    else:
-        indi_table_row.append("No")
-        indi_table_row.append(indi_info["Death Date"])
-    child = next((fam_id for fam_id, fam_info in families.items() if indi_id in fam_info.get("Children", [])), "-")
-    indi_table_row.append(child)
-    spouse = next((fam_id for fam_id, fam_info in families.items() if indi_id == fam_info.get("Husband ID") or indi_id == fam_info.get("Wife ID")), "-")
-    indi_table_row.append(spouse)
-    individual_table.add_row(indi_table_row)
+    # Display family information in a table
+    family_table = PrettyTable()
+    family_table.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
 
-# Display family information in a table
-family_table = PrettyTable()
-family_table.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
+    for fam_id, fam_info in families.items():
+        fam_table_row = [fam_id]
+        if fam_info.get("Married"):
+            fam_table_row.append(fam_info.get("Marriage Date"))
+        else:
+            fam_table_row.append("No")
+        if fam_info.get("Divorced"):
+            fam_table_row.append(fam_info.get("Divorce Date"))
+        else:
+            fam_table_row.append("No")
+        husband_name = individuals.get(fam_info["Husband ID"], {}).get("Name", "-")
+        wife_name = individuals.get(fam_info["Wife ID"], {}).get("Name", "-")
+        fam_table_row.append(fam_info["Husband ID"])
+        fam_table_row.append(husband_name)
+        fam_table_row.append(fam_info["Wife ID"])
+        fam_table_row.append(wife_name)
+        children = ", ".join(individuals.get(child_id, {}).get("Name", "-") for child_id in fam_info.get("Children", []))
+        fam_table_row.append(children)
+        family_table.add_row(fam_table_row)
 
-for fam_id, fam_info in families.items():
-    fam_table_row = [fam_id]
-    if fam_info.get("Married"):
-        fam_table_row.append(fam_info.get("Marriage Date"))
-    else:
-        fam_table_row.append("No")
-    if fam_info.get("Divorced"):
-        fam_table_row.append(fam_info.get("Divorce Date"))
-    else:
-        fam_table_row.append("No")
-    husband_name = individuals.get(fam_info["Husband ID"], {}).get("Name", "-")
-    wife_name = individuals.get(fam_info["Wife ID"], {}).get("Name", "-")
-    fam_table_row.append(fam_info["Husband ID"])
-    fam_table_row.append(husband_name)
-    fam_table_row.append(fam_info["Wife ID"])
-    fam_table_row.append(wife_name)
-    children = ", ".join(individuals.get(child_id, {}).get("Name", "-") for child_id in fam_info.get("Children", []))
-    fam_table_row.append(children)
-    family_table.add_row(fam_table_row)
+    # Print the tables
+    print("Individuals:")
+    print(individual_table)
 
-# Print the tables
-print("Individuals:")
-print(individual_table)
+    print("\nFamilies:")
+    print(family_table)
 
-print("\nFamilies:")
-print(family_table)
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+   main()
