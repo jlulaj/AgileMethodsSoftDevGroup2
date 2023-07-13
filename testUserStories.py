97348342d21f804b557d4999acea0be3d32049ad
kayligregory <kgregory@stevens.edu>
2023-07-13 19:05:41 -0400

    test stories 04,05,07,38

--- testUserStories.py
+++ testUserStories.py
@@ -18,6 +18,18 @@ class storyTest(unittest.TestCase):
     def test_us02(self):
         self.assertEqual([], userStories.us02(families, individuals))
 
+    # US04
+    def test_us04(self):
+        self.assertEqual([], userStories.us04(families, individuals))
+
+    # US05
+    def test_us05(self):
+        self.assertEqual([], userStories.us05(families, individuals))
+
+    # US07
+    def test_us07(self):
+        self.assertEqual([], userStories.us07(families, individuals))
+
     # US15
     def test_us15(self):
         self.assertEqual([], userStories.us15(families, individuals))
@@ -41,6 +53,10 @@ class storyTest(unittest.TestCase):
     # US35
     def test_us35(self):
         self.assertEqual([], userStories.us35(families, individuals))
+    
+    # US38
+    def test_us38(self):
+        self.assertEqual([], userStories.us38(families, individuals))
 
     # US39
     def test_us39(self):
